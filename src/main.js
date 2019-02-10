764d4826fd1a9d8693ba5ad5fc58ad3c886bfe43
JavaDogDev <JavaDog.Dev@gmail.com>
2019-02-09 19:13:05 -0500

    Fix eslint problems

--- src/main.js
+++ src/main.js
@@ -1,33 +1,42 @@
 import Cookies from 'js-cookie';
 import './index.scss';
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', () => {
   const UPDATE_FREQUENCY_MS = 100;
 
-  /** Screen 1 **/
-  const screen1 = document.getElementById("container-screen1");
-  const screen2 = document.getElementById("container-screen2");
-  const form = document.getElementById("data-form");
-  const checkbox_includeTaxes = document.getElementById("include-taxes");
-  const input_taxes = document.getElementById("annual-tax");
-  const checkbox_alreadyWorked = document.getElementById("already-worked-some");
-  const input_workedHours = document.getElementById("already-worked-hours");
+  /* Screen 1 */
+  const screen1 = document.getElementById('container-screen1');
+  const screen2 = document.getElementById('container-screen2');
+  const form = document.getElementById('data-form');
+  const checkboxIncludeTaxes = document.getElementById('include-taxes');
+  const inputTaxes = document.getElementById('annual-tax');
+  const checkboxAlreadyWorked = document.getElementById('already-worked-some');
+  const inputWorkedHours = document.getElementById('already-worked-hours');
 
-  checkbox_includeTaxes.addEventListener("change", function() {
-    input_taxes.disabled = !this.checked;
-    input_taxes.required = this.checked;
+  checkboxIncludeTaxes.addEventListener('change', function handleCheck() {
+    inputTaxes.disabled = !this.checked;
+    inputTaxes.required = this.checked;
   });
 
-  checkbox_alreadyWorked.addEventListener("change", function() {
-    input_workedHours.disabled = !this.checked;
-    input_workedHours.required = this.checked;
+  checkboxAlreadyWorked.addEventListener('change', function handleCheck() {
+    inputWorkedHours.disabled = !this.checked;
+    inputWorkedHours.required = this.checked;
   });
+
+  function roundNum(value, decimals) {
+    // eslint-disable-next-line prefer-template
+    return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals).toFixed(decimals);
+  }
+
+  function fireChangeEvent(inputField) {
+    inputField.dispatchEvent(new UIEvent('change'));
+  }
 
   // Load previous data from cookie
-  const cookieData = Cookies.getJSON("res-data");
-  if(typeof cookieData !== "undefined" && cookieData.hasOwnProperty("annualIncome")) {
-    Object.keys(cookieData).forEach( (key) => {
-      if(form.elements[key].type === "checkbox") {
+  const cookieData = Cookies.getJSON('res-data');
+  if (typeof cookieData !== 'undefined' && cookieData.hasOwnProperty('annualIncome')) {
+    Object.keys(cookieData).forEach((key) => {
+      if (form.elements[key].type === 'checkbox') {
         form.elements[key].checked = cookieData[key];
         fireChangeEvent(form.elements[key]);
       } else {
@@ -36,39 +45,40 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   }
 
-  form.addEventListener("submit", function(event) {
+  form.addEventListener('submit', (event) => {
     event.preventDefault();
 
     // Save entered data to cookie
     const newCookie = {};
     const excludeKeys = new Set([
-      "already-worked-some",
-      "alreadyWorkedSome",
-      "already-worked-hours",
-      "alreadyWorkedHours"
+      'already-worked-some',
+      'alreadyWorkedSome',
+      'already-worked-hours',
+      'alreadyWorkedHours',
     ]);
     // forms have numeric keys we have to filter out
     const numberRegex = /^\d+$/;
-    Object.keys(form.elements).forEach( (key) => {
-      if(!excludeKeys.has(key) && !numberRegex.test(key)) {
-        if(form.elements[key].type === "checkbox") {
+    Object.keys(form.elements).forEach((key) => {
+      if (!excludeKeys.has(key) && !numberRegex.test(key)) {
+        if (form.elements[key].type === 'checkbox') {
           newCookie[key] = form.elements[key].checked;
         } else {
           newCookie[key] = form.elements[key].value;
         }
       }
     });
-    Cookies.set("res-data", newCookie, {expires: 365});
+    Cookies.set('res-data', newCookie, { expires: 365 });
 
+    // eslint-disable-next-line no-use-before-define
     initScreen2();
 
     // Launch second screen
-    screen1.addEventListener("transitionend", function(event) {
-      if(event.propertyName === "opacity") {
-        screen1.style.display = "none";
+    screen1.addEventListener('transitionend', (e) => {
+      if (e.propertyName === 'opacity') {
+        screen1.style.display = 'none';
 
-        screen2.style.display = "flex";
-        window.setTimeout(function() {
+        screen2.style.display = 'flex';
+        window.setTimeout(() => {
           screen2.style.opacity = 1;
         }, 100);
       }
@@ -76,28 +86,28 @@ document.addEventListener("DOMContentLoaded", function() {
     screen1.style.opacity = 0;
   });
 
-  /** Screen 2 **/
+  /* Screen 2 */
   function initScreen2() {
-    const sinceStartingText = document.getElementById("since-starting-text");
-    const cashDisplay = document.getElementById("cash-display");
-    const taxesPaidArea = document.getElementById("taxes-paid");
-    const taxesPaidDisplay = document.getElementById("taxes-paid-display");
+    const sinceStartingText = document.getElementById('since-starting-text');
+    const cashDisplay = document.getElementById('cash-display');
+    const taxesPaidArea = document.getElementById('taxes-paid');
+    const taxesPaidDisplay = document.getElementById('taxes-paid-display');
 
     const startTime = Date.now();
-    const workdaysPerWeek = parseInt(form.workdaysPerWeek.value);
-    const workHoursPerDay = parseInt(form.hoursPerDay.value);
+    const workdaysPerWeek = parseInt(form.workdaysPerWeek.value, 10);
+    const workHoursPerDay = parseInt(form.hoursPerDay.value, 10);
     const workHoursPerYear = (workdaysPerWeek * workHoursPerDay) * 52.1775; // weeks per year
     const hourlyIncome = parseFloat(form.annualIncome.value) / workHoursPerYear;
     const hourlyTax = parseFloat(form.annualTax.value) / workHoursPerYear;
     const workedHours = parseFloat(form.alreadyWorkedHours.value);
 
     sinceStartingText.innerHTML = form.alreadyWorkedSome.checked
-      ? "So far today, you've made:"
-      : "Since pressing start, you've made:";
+      ? 'So far today, you\'ve made:'
+      : 'Since pressing start, you\'ve made:';
 
     taxesPaidArea.style.display = form.includeTaxes.checked
-      ? "block"
-      : "none";
+      ? 'block'
+      : 'none';
 
     function moneyPerCentisecond(cashPerHour, startingTime) {
       // a centisecond is 1/100 of one second, and there are 360000 of them per hour
@@ -110,41 +120,34 @@ document.addEventListener("DOMContentLoaded", function() {
       let income = moneyPerCentisecond(hourlyIncome, startTime);
       let taxes = 0;
 
-      if(form.includeTaxes.checked) {
+      if (form.includeTaxes.checked) {
         taxes = moneyPerCentisecond(hourlyTax, startTime);
         income -= taxes;
       }
 
-      if(form.alreadyWorkedSome.checked) {
+      if (form.alreadyWorkedSome.checked) {
         income += (workedHours * hourlyIncome);
 
-        if(form.includeTaxes.checked) {
+        if (form.includeTaxes.checked) {
           const pastTax = (workedHours * hourlyTax);
           income -= pastTax;
           taxes += pastTax;
         }
       }
 
-      if(form.includeTaxes.checked) {
-        taxesPaidDisplay.innerHTML = "$" + roundNum(
-            taxes,
-            (form.displayFractionalPennies.checked) ? 4 : 2);
+      if (form.includeTaxes.checked) {
+        // eslint-disable-next-line prefer-template
+        taxesPaidDisplay.innerHTML = '$' + roundNum(
+          taxes,
+          (form.displayFractionalPennies.checked) ? 4 : 2);
       }
 
-      cashDisplay.innerHTML = "$" + roundNum(
+      // eslint-disable-next-line prefer-template
+      cashDisplay.innerHTML = '$' + roundNum(
         income,
         (form.displayFractionalPennies.checked) ? 4 : 2);
     }
 
     setInterval(updateDisplay.bind(this), UPDATE_FREQUENCY_MS);
-  }
-
-  function roundNum(value, decimals) {
-    // gotta love javascript
-    return Number(Math.round(value+'e'+decimals)+'e-'+decimals).toFixed(decimals);
-  }
-
-  function fireChangeEvent(inputField) {
-    inputField.dispatchEvent(new UIEvent("change"));
   }
 });
